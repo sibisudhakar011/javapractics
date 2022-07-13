cec623a8388880d262d431f3ebe7051f80d6da85
sibisudhakar011 <86553798+sibisudhakar011@users.noreply.github.com>
2022-07-13 14:13:09 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,15 @@
+public class pattern printing {
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    int n=4;
+    for(int i=0;i<n;i++){
+        for(var j=4;j>=1;j--){
+            System.out.print{"*"};
+
+        }
+        System.out.println();
+    }
+
+}
